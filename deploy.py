4022da376806aafed0f825362b22b7cc9c28407c
李彥寬 <yenkuanlee@liyankuandeMacBook-Pro.local>
2018-03-30 17:33:10 +0800

    first commit

--- deploy.py
+++ deploy.py
@@ -4,8 +4,8 @@ DomainName = sys.argv[1]
 MnodeIP = sys.argv[2]
 DnodeIP = sys.argv[3]
 
+os.chdir("iServStorPKG")
 os.system("sed -i 's/DATA_NODE_IP=/DATA_NODE_IP="+DnodeIP+"/g' iservstor.rc")
 os.system("sed -i 's/MANAGEMENT_NODE_IP=/MANAGEMENT_NODE_IP="+MnodeIP+"/g' iservstor.rc")
 os.system("sed -i 's/ISERVSTOR_DOMAIN_NAME=/ISERVSTOR_DOMAIN_NAME="+DomainName+"/g' iservstor.rc")
-os.chdir("iServStorPKG")
 os.system("./deploy install")
