89aa03dc099ae0f5b5b5a19960537381ef919cc7
李彥寬 <yenkuanlee@liyankuandeMacBook-Pro.local>
2018-03-30 14:41:25 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -f iServStorARC.tar.gz iServStorPKG.tar.gz
+sudo ntpdate time.stdtime.gov.tw
+path="http://140.92.143.210/release/iservstor"
+for i in "iServStorARC" "iServStorPKG"; do
+    wget --http-user=iservstor --http-passwd=iservstor $path/${i}.tar.gz
+done
+tar zxf iServStorPKG.tar.gz
+cp iservstor.rc iServStorPKG
+cd iServStorPKG
+chmod +x deploy
+./deploy install
